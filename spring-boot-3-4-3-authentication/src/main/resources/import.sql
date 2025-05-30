ba8f54dcf4ebc2c30615be360214ee5139b4dd26
Artur Ragazzi <ao.ragazzi@gmail.com>
2025-05-30 15:47:56 -0300

    11 Bcript Finalizado

--- spring-boot-3-4-3-authentication/src/main/resources/import.sql
+++ spring-boot-3-4-3-authentication/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$ii7I37xvCjst4P0G6PxhX.Jcrl10YI1.GFKWbw7EuF2nd/orC1IGK');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$ii7I37xvCjst4P0G6PxhX.Jcrl10YI1.GFKWbw7EuF2nd/orC1IGK');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR')
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN')
